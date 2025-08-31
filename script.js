0c6cfd6125678476abe713a02a5779919b4449db
mayankkhanna <mayankkhanna701@gmail.com>
2025-08-31 14:10:42 +0530

    Initial commit - Rock Paper Scissors game

---
+++ script.js
@@ -0,0 +1,41 @@
+let userscore = 0;
+let compscore = 0;
+const choices = document.querySelectorAll(".image");
+document.getElementById("userscore").textContent=userscore;
+document.getElementById("compscore").textContent=compscore;
+function getcompchoice(){
+    let compchoice = Math.floor(Math.random()*3);
+    if(compchoice==0){
+        return "Rock";
+    }
+    if(compchoice==1){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+}
+function playround(userchoice,compchoice){
+    if(userchoice===compchoice){
+        return "It was a draw You both chose "+compchoice;
+    }
+    if(userchoice=="Rock"&&compchoice=="Scissors"||userchoice=="Paper"&&compchoice=="Rock"||userchoice=="Scissors"&&compchoice=="Paper")
+    {
+        userscore++;
+        return "YOU WON!! "+userchoice+" beats "+compchoice;
+    }
+    else{
+        compscore++;
+        return "YOU LOSE!! "+compchoice+" beats "+userchoice;
+    }
+}
+choices.forEach( choice =>{
+choice.addEventListener("click", () =>{
+    userchoice=choice.id;
+    compchoice=getcompchoice();
+    result=playround(userchoice,compchoice);
+    document.getElementById("userscore").textContent = userscore;
+    document.getElementById("compscore").textContent = compscore;
+    document.getElementById("pyc").textContent = result;
+});
+});
